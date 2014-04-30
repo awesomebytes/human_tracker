7e919b975cf95ead304649c53ab5aaf13628c853
Víctor López <victor.lopez@pal-robotics.com>
2014-04-30 12:48:43 +0200

    Fix compilation warning that were converted to error

--- packages/object_tracking/src/tracker.cpp
+++ packages/object_tracking/src/tracker.cpp
@@ -1,12 +1,12 @@
 /*
 Software License Agreement (BSD License)
- 
+
 Copyright (c) 2013, Southwest Research Institute
 All rights reserved.
- 
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
- 
+
    * Redistributions of source code must retain the above copyright
      notice, this list of conditions and the following disclaimer.
    * Redistributions in binary form must reproduce the above copyright
@@ -15,7 +15,7 @@ modification, are permitted provided that the following conditions are met:
    * Neither the name of the Southwest Research Institute, nor the names
      of its contributors may be used to endorse or promote products derived
      from this software without specific prior written permission.
- 
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -159,12 +159,12 @@ int ParticleTrack::update_state(cv::Mat& input, cv::Mat& disp, double dt)
 
   // update filter state (will use passed through Mat objects in likelihood fcn)
   if (debug_mode_)
-    ROS_INFO("Updating object %d, dt = %f", uid, dt);
+    ROS_INFO("Updating object %llu, dt = %f", uid, dt);
   updateState(input, dt);
   if (is_dead_)
   {
     if (debug_mode_)
-      ROS_INFO("Tracker %d died.", uid);
+      ROS_INFO("Tracker %llu died.", uid);
     return is_dead_;
   }
 
